4ef700f5a633ba29b4e641fdddbd30194f44ca3a
bunty <karthik.patel06@gmail.com>
2023-01-10 22:02:05 +0530

    Completed Load Extensions Module

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from ssg import extensions
 
 
 class Site:
@@ -27,12 +28,14 @@ class Site:
             )
 
     def build(self):
+        extensions.load_bundled()
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+
 
     @staticmethod
     def error(message):
